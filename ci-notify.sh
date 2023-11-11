037521e19fe56217206c7b5f5f435123fb1358ab
Denis <kadakava@yandex.ru>
2023-11-11 22:39:07 +0300

    edited script

--- ci-notify.sh
+++ ci-notify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TIME="10"
-'curl -X POST -H "Content-type: application/json" --data "{\"chat_id\": \"${CHAT_ID}\", \"text\": \"test\"}" ${TELEGRAM_WEB_HOOK}'
+'curl -X POST -H "Content-type: application/json" - "{\"chat_id\": \"${CHAT_ID}\", \"text\": \"test\"}" ${TELEGRAM_WEB_HOOK}'
 
 #URL="https://api.telegram.org/bot5933756043:AAE8JLL5KIzgrNBeTP5e-1bkbJy4YRoeGjs/sendMessage"
 #TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
