f6a943457aa8451b03d3fe6a44e427ac93f67f10
Denis <kadakava@yandex.ru>
2023-12-13 22:44:33 +0300

    send notification

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,7 +2,7 @@
 #Если свалится одна из команд, рухнет и весь скрипт
 set -xe
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
-sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo cp -rf backend.service /etc/systemd/system/backend.service
 sudo rm -f /home/jarservice/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
