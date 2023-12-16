f5f6f01315e83689424414800fad00c50daf4000
Denis <kadakava@yandex.ru>
2023-12-16 23:32:43 +0300

    send notification

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -5,7 +5,7 @@ set -xe
 sudo cp -rf frontend.service /etc/systemd/system/frontend.service
 sudo rm -rf /var/www-data/dist/frontend||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/${NEXUS_REPO_FRONTEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.tar.gz
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.tar.gz ${NEXUS_REPO_URL}/${NEXUS_REPO_FRONTEND_NAME}/sausage-store-${VERSION}.tar.gz
 sudo tar -xvzf ./sausage-store.tar.gz -C /var/www-data/dist/||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
