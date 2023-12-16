173bcb3091e9ae8669c0dbcfb025af793b14a60d
Denis <kadakava@yandex.ru>
2023-12-16 21:47:05 +0300

    send notification

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -7,8 +7,8 @@ sudo rm -f /var/www-data/bin/sausage-store.jar||true
 #Переносим артефакт в нужную папку
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/sausage-store-backend/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /var/www-data/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
-sudo chown -R backend:backend /var/www-data/bin/
-sudo chmod ugo+x /var/www-data/bin/sausage-store.jar
+#sudo chown -R backend:backend /var/www-data/bin/
+#sudo chmod ugo+x /var/www-data/bin/sausage-store.jar
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
 #Перезапускаем сервис сосисочной
